9e98aebf2bf640c999c49b0c5c976fb75a2324f3
wheejuni <wheejuni@gmail.com>
2017-03-01 03:25:59 +0000

    -요소의 종류

--- rlecture1.R
+++ rlecture1.R
@@ -3,3 +3,13 @@ ht <- c(123, 124, 125, 126, 127)
 gend <-c("남" , "여", "남", "여")
 
 
+vec_logical <- c(TRUE, FALSE, TRUE, TRUE)
+vec_integer <- c(1, 3, 4)
+vec_double <- c(1.2, 1.3, 1.4)
+vec_character <- c("1", "2", "3", "4")
+
+vec_logical
+typeof(vec_double)
+typeof(vec_character)
+typeof(vec_logical)
+typeof(vec_integer)
