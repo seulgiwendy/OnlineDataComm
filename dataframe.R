d6fb98c754f7d12eb2ea63da6b42cd1700413ae8
wheejuni <wheejuni@gmail.com>
2017-03-16 15:57:51 +0900

    -dataframe 예제

---
+++ dataframe.R
@@ -0,0 +1,8 @@
+#dataframe example
+
+no = c(1, 2, 3, 4)
+name = c("apple", "peach", "banana", "grape")
+price = c(500, 200, 120, 50)
+qty = c(5, 4, 3, 2)
+sales = data.frame(NO = no, NAME = name, PR = price, Q = qty)
+sales
