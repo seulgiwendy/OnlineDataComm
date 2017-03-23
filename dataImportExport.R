9247915b1ec25886b8112a01702fdb142f521b58
wheejuni <wheejuni@gmail.com>
2017-03-23 15:31:14 +0900

    -data import

--- dataImportExport.R
+++ dataImportExport.R
@@ -1,3 +1,7 @@
 list.files()
 fruits = read.table("fruits.txt")
-fruits = read.table("fruits.txt", header = T)
+fruits = read.table("fruits.txt", header = T) #making first row as a label 
+fruit2 = read.table("fruits.txt", header = T, nrow = 5) #cut
+fruit3 = read.table("fruits.txt", header = T, skip = 5)
+fruitsucks = read.table("fruits.txt", header = T, skip = 8) #skip until nth row
+
--- dataframe.R
+++ dataframe.R
@@ -6,3 +6,5 @@ price = c(500, 200, 120, 50)
 qty = c(5, 4, 3, 2)
 sales = data.frame(NO = no, NAME = name, PR = price, Q = qty)
 sales
+
+
