794bec68bf99df57c3d747a47bc7b8ab594d833f
wheejuni <wheejuni@gmail.com>
2017-03-26 11:20:36 +0000

    -import 내용 추가

--- 20170330/anotherSubway.R
+++ 20170330/anotherSubway.R
@@ -1,3 +1,4 @@
+data1 = read.csv("1-4호선승하차승객수.csv")
 ridingByLines = aggregate(승차~노선번호, data1, sum)
 gettingOffByLines= aggregate(하차~노선번호, data1, sum)
 withoutLines = apply(data1[,c(3,4)], 2, sum)
--- 20170330/fruit.R
+++ 20170330/fruit.R
@@ -1,3 +1,4 @@
+fruitsales = read.csv("과일판매현황.csv")
 aggregate(판매금액~이름, fruitsales, sum)
 fruitsales_branch = fruitsales[c(2,4),c(2,4)]
 apply(fruitsales_branch, 2, sum)
