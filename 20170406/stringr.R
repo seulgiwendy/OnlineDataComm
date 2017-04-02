5aabc09a4114232beda033c1546eb1014ea3c8e6
wheejuni <wheejuni@gmail.com>
2017-04-02 12:34:41 +0000

    -unlist 함수 추가

--- 20170406/stringr.R
+++ 20170406/stringr.R
@@ -2,3 +2,5 @@ str_replace_all(string1, '/', '-' )
 str_split(string1, "/")
 tel2 = str_split(tel1, "\\)")
 tel2
+telarray = unlist(tel2)
+telarray[1] #extracts the first three numbers of telephone number
