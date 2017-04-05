0ff06c34bfabe95435fb99713e2e60e4a80d3e4a
wheejuni <wheejuni@gmail.com>
2017-04-05 00:25:19 +0000

    -str4 해결

--- 20170406/stringr.R
+++ 20170406/stringr.R
@@ -1,6 +1,12 @@
+string1 = readLines("stringr연습문제.txt")
 str_replace_all(string1, '/', '-' )
 str_split(string1, "/")
 tel2 = str_split(tel1, "\\)")
 tel2
 telarray = unlist(tel2)
 telarray[1] #extracts the first three numbers of telephone number
+#str4
+str1 = str_split(string1, "/")
+str1_array = unlist(str1)
+names(str1_array) = str1_array
+str1_array
